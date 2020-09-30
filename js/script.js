879d88f0689a1ada56ff19c6359607e320a9030b
edrivasclemente <erickrivs@gmail.com>
2020-09-30 16:30:59 -0400

    ini

--- js/script.js
+++ js/script.js
@@ -217,33 +217,3 @@ $(document).ready(function() {
   });
 });
 //FIN SLIDER=======
-
-//CAROUSEL OWL===========
-//inicializando la clase========
-
-$(document).ready(function(){
-  $(".owl-carousel").owlCarousel();
-});
-//finalizando la clase========
-
-$('.owl-carousel').owlCarousel({
-  loop:true,
-  margin:10,
-  responsiveClass:true,
-  responsive:{
-      0:{
-          items:1,
-          nav:true
-      },
-      600:{
-          items:3,
-          nav:false
-      },
-      1000:{
-          items:5,
-          nav:true,
-          loop:false
-      }
-  }
-})
-//FIN CAROUSEL OWL===========
